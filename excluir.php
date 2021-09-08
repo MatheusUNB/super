8e858d3c12aa4e18128ec409cd0eaa01464be5ae
matheusunb <alextexugo@gmail.com>
2021-09-08 14:29:02 -0300

    atualizações

--- excluir.php
+++ excluir.php
@@ -2,9 +2,9 @@
 session_start();
 include("conexao.php");
 
-$usuario_id = mysqli_real_escape_string($conexao, $_GET['usuario_id']);
+$usuario_id = mysqli_real_escape_string($conexao, $_POST['usuario_id']);
 
-$sql = "DELETE * FROM contato WHERE usuario_id = '$usuario_id'";
+$sql = "DELETE FROM contato WHERE usuario_id = '$usuario_id'";
 
 if($conexao->query($sql) === TRUE) {
 	$_SESSION['status_excluir'] = true;
@@ -12,6 +12,6 @@ if($conexao->query($sql) === TRUE) {
 
 $conexao->close();
 
-header('Location: home.php');
+header('Location: consulta.php');
 exit;
 ?>
